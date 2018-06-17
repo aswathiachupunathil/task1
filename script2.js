1cc68c89597951563e08f6680899aad4817401f4
Nadeem_Shadan <nadeem.s@iinerds.com>
2018-06-17 21:21:42 +0530

    task 1

---
+++ script2.js
@@ -0,0 +1,13 @@
+function pop(){
+var btn = document.getElementById("myBtn");
+var modal = document.getElementById('myModal');
+modal.style.display = "block";
+
+}
+
+
+function closebutton2(){
+var modal = document.getElementById('myModal');
+var span = document.getElementsByClassName("close")[0];
+modal.style.display = "none";
+}
